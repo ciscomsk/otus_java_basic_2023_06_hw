cdb755d2a8f0c6bc860a8501d15726722e2ba3e7
ciscomsk <kuznetzoff@gmail.com>
2023-08-15 11:33:40 +0300

    Fix review comments

--- src/main/java/ru/otus/hw/l_3/HW.java
+++ src/main/java/ru/otus/hw/l_3/HW.java
@@ -30,7 +30,7 @@ public class HW {
 
         if (data <= 10) {
             System.out.println("Красный");
-        } else if (data > 10 && data <= 20) {
+        } else if (data <= 20) {
             System.out.println("Желтый");
         } else {
             System.out.println("Зеленый");
