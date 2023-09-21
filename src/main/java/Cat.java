31cf060a4b5a86618ef2b9d56ef65d966a005fb5
ciscomsk <kuznetzoff@gmail.com>
2023-09-21 16:53:35 +0300

    Added hw for lesson 11

---
+++ src/main/java/Cat.java
@@ -0,0 +1,5 @@
+public class Cat extends Animal {
+    public Cat(String name, int endurance, int runSpeed, AnimalState state) {
+        super(name, endurance, runSpeed, state);
+    }
+}
---
+++ src/main/java/Dog.java
@@ -0,0 +1,39 @@
+public class Dog extends Animal implements CanSwim {
+    private static final int ENDURANCE_LOSS_SWIM = 2;
+    private int swimSpeed;
+    public Dog(String name, int endurance, int runSpeed, int swimSpeed, AnimalState state) {
+        super(name, endurance, runSpeed, state);
+        this.swimSpeed = swimSpeed;
+    }
+
+    @Override
+    public int swim(int distance) {
+        int time = -1;
+        if (endurance == 0) {
+            printTiredState(name);
+            return time;
+        }
+
+        int enduranceLoss = distance * ENDURANCE_LOSS_SWIM;
+
+        if (enduranceLoss > endurance) {
+            float distanceBeforeTired = (float) endurance / ENDURANCE_LOSS_SWIM;
+            float timeBeforeTired = distanceBeforeTired / swimSpeed;
+            endurance = 0;
+            System.out.println(name + " swam " + distanceBeforeTired + " out of " + distance + "m in " + timeBeforeTired + "s");
+            state = AnimalState.TIRED;
+            printTiredState(name);
+            return time;
+        }
+
+        endurance -= enduranceLoss;
+        time = distance / swimSpeed;
+        System.out.println(name + " swam " + distance + "m in " + time + "s");
+        if (endurance == 0) {
+            state = AnimalState.TIRED;
+            printTiredState(name);
+        }
+
+        return time;
+    }
+}
